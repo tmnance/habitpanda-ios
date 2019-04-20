18941bda04d224fa15cf204dbce591b68d972935
Tim Nance <tmnance@gmail.com>
2019-04-20 13:05:28 -0400

    Add UITextField max length support

---
+++ HabitPanda/SupportFiles/Extensions/UITextField+MaxLength.swift
@@ -0,0 +1,44 @@
+//
+//  UITextField+MaxLength.swift
+//  HabitPanda
+//
+//  Created by Tim Nance on 4/20/19.
+//  Copyright © 2019 Tim Nance. All rights reserved.
+//
+
+import UIKit
+
+private var kAssociationKeyMaxLength: Int = 0
+
+extension UITextField {
+
+    @IBInspectable var maxLength: Int {
+        get {
+            if let length = objc_getAssociatedObject(self, &kAssociationKeyMaxLength) as? Int {
+                return length
+            } else {
+                return Int.max
+            }
+        }
+        set {
+            objc_setAssociatedObject(self, &kAssociationKeyMaxLength, newValue, .OBJC_ASSOCIATION_RETAIN)
+            addTarget(self, action: #selector(checkMaxLength), for: .editingChanged)
+        }
+    }
+
+    @objc func checkMaxLength(textField: UITextField) {
+        guard let prospectiveText = self.text,
+            prospectiveText.count > maxLength
+            else {
+                return
+        }
+
+        let selection = selectedTextRange
+
+        let indexEndOfText = prospectiveText.index(prospectiveText.startIndex, offsetBy: maxLength)
+        let substring = prospectiveText[..<indexEndOfText]
+        text = String(substring)
+
+        selectedTextRange = selection
+    }
+}
