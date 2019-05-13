20cfc54141380d2896592e32f69caf0217032f61
Tim Nance <tmnance@gmail.com>
2019-05-13 17:24:08 -0400

    Add check-in data model

---
+++ HabitPanda/Models/CheckIn.swift
@@ -0,0 +1,14 @@
+//
+//  CheckIn.swift
+//  HabitPanda
+//
+//  Created by Tim Nance on 5/13/19.
+//  Copyright © 2019 Tim Nance. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(CheckIn)
+public class CheckIn: NSManagedObject {
+}
